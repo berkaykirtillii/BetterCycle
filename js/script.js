5c0ba387efae2a9b08c6d9b5135ee546fc638257
berkaykirtilii <16050111037@ybu.edu.tr>
2021-04-13 16:02:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function login(form){
+  if(form.loginMail.value=="user12@gmail.com" && form.loginPassword.value=="qwerty"){
+    window.open("index2.html","_self")
+  }
+  else{
+    alert("The username and password dont match!")
+  }
+}
+
+$('.partners-logos').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    autoplay:true,
+    autoplaySpeed: 1500,
+    infinite: true,
+  });
+
+function openLogin(){
+  $('.login-page').toggleClass('login-panel-show');
+  $('#loginBtn').toggleClass('login-btn-hide'); 
+}
+
+
+  $('.container').click(function(){
+    if($('#loginID').hasClass('login-panel-show')){
+      $('.container').click(function(){
+        $('.login-page').toggleClass('login-panel-show');
+        $('#loginBtn').toggleClass('login-btn-hide'); 
+      });
+    }  
+  });
+
+
