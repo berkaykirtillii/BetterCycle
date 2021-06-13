8222f8ca99a3bc8e2465f353b743134bb3d3a493
berkaykirtilii <16050111037@ybu.edu.tr>
2021-06-13 23:44:59 +0300

    jg

--- json/json.js
+++ json/json.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
          
       function renderHTML(data){
           var htmlString="";    
-          htmlString+="<h5>" + data.name + "</h5>";
-          htmlString+="<p class='title'>" + data.title + "<p>";
-          htmlString+="<p>" + data.about + "</p>";
+          htmlString+="<h5>" + data[0].name + "</h5>";
+          htmlString+="<p class='title'>" + data[0].title + "<p>";
+          htmlString+="<p>" + data[0].about + "</p>";
           personalInfo.innerHTML = htmlString;    
           
           ourData = ""
