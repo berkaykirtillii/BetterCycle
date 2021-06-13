a567cb7529948fe690eb61fad82498529dc91cfc
berkaykirtilii <16050111037@ybu.edu.tr>
2021-06-13 22:01:02 +0300

    changes

--- json/json.js
+++ json/json.js
@@ -24,12 +24,11 @@ $(document).ready(function() {
           ourData = ""
       }
 
-
       
       var commentsText = document.getElementById("comments-text");
 
       var commentsRequest = new XMLHttpRequest();
-      commentsRequest.open("GET","https://sprintberkay.herokuapp.com/json/allow-origin.php",true);
+      commentsRequest.open("GET","https://sprintberkay.herokuapp.com/json/comments.json",true);
 
       commentsRequest.onload=function(){
           var textData=JSON.parse(infoRequest.responseText);
@@ -40,12 +39,14 @@ $(document).ready(function() {
 
       function renderCommentHTML(data){
           var text = "";
-          text+="<div class='col-md-5 offset-1'><div class='comment'><p>"+
-           data.comments[0].comment+ "<span><i id='like' class='fas fa-angle-double-up'>"+
-           "<i id='likeCounter' class='text-normal'>13</i></i>"+
-           "<i id='dislike' class='fas fa-angle-double-down'>"+
-           "<i id='dislikeCounter' class='text-normal'>2</i></i>"+
-           "</span></div>";
+          for(let i = 0 ; i < data.length ;i++){
+            text+="<div class='col-md-5 offset-1'><div class='comment'><p>"+
+            data.comments[i].comment+ "<span><i id='like' class='fas fa-angle-double-up'>"+
+            "<i id='likeCounter' class='text-normal'>13</i></i>"+
+            "<i id='dislike' class='fas fa-angle-double-down'>"+
+            "<i id='dislikeCounter' class='text-normal'>2</i></i>"+
+            "</span></div>";
+          }
            commentsText.innerHTML=text;
       }
       
