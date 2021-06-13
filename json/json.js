66e02e225cdb4447df5ecac09aace3bcc9174b04
berkaykirtilii <16050111037@ybu.edu.tr>
2021-06-13 22:43:41 +0300

    a

--- json/json.js
+++ json/json.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
       var personalInfo = document.getElementById("personal-info");
 
       var infoRequest= new XMLHttpRequest();
-      infoRequest.open("GET","cors-internal.php",true);
+      infoRequest.open("GET","personal-info.json",true);
      
       infoRequest.onload=function(){
       var ourData=JSON.parse(infoRequest.responseText);
@@ -22,6 +22,30 @@ $(document).ready(function() {
           ourData = ""
       }
 
-     
+      
+      var commentsText = document.getElementById("comments-text");
+
+      var commentsRequest = new XMLHttpRequest();
+      commentsRequest.open("GET","https://sprintberkay.herokuapp.com/json/cors-external.php",true);
+
+      commentsRequest.onload=function(){
+          var textData=JSON.parse(commentsRequest.responseText);
+          renderCommentHTML(textData);
+      };
+
+      commentsRequest.send();
+
+      function renderCommentHTML(data){
+          var text = "";
+          for(let i = 0 ; i < data.length ;i++){
+            text+="<div class='col-md-5 offset-1'><div class='comment'><p>"+
+            data.comments[i].comment+ "<span><i id='like' class='fas fa-angle-double-up'>"+
+            "<i id='likeCounter' class='text-normal'>13</i></i>"+
+            "<i id='dislike' class='fas fa-angle-double-down'>"+
+            "<i id='dislikeCounter' class='text-normal'>2</i></i>"+
+            "</span></div>";
+          }
+           commentsText.innerHTML=text;
+      }
       
   }); 
