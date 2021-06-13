4631bfdabea86e74318a9ea80c24844d312fe764
berkaykirtilii <16050111037@ybu.edu.tr>
2021-06-13 22:21:30 +0300

    changes

---
+++ json/allow-origin-external.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: multipart/form-data');
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+echo file_get_contents('comments.json'); 
--- json/json.js
+++ json/json.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
       var infoRequest= new XMLHttpRequest();
       infoRequest.overrideMimeType("application/json");
-      infoRequest.open("GET","json/personal-info.json",true);
+      infoRequest.open("GET","json/allow-origin.php",true);
      
       infoRequest.onload=function(){
       var ourData=JSON.parse(infoRequest.responseText);
@@ -28,7 +28,7 @@ $(document).ready(function() {
       var commentsText = document.getElementById("comments-text");
 
       var commentsRequest = new XMLHttpRequest();
-      commentsRequest.open("GET","https://sprintberkay.herokuapp.com/json/comments.json",true);
+      commentsRequest.open("GET","https://sprintberkay.herokuapp.com/json/allow-origin-external.php",true);
 
       commentsRequest.onload=function(){
           var textData=JSON.parse(infoRequest.responseText);
