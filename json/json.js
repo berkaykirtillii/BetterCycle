3461ed32adbd43536dd1db758bb2a8ca9248c9cb
berkaykirtilii <16050111037@ybu.edu.tr>
2021-06-13 23:34:18 +0300

    a

--- json/json.js
+++ json/json.js
@@ -14,9 +14,9 @@ $(document).ready(function() {
          
       function renderHTML(data){
           var htmlString="";    
-          htmlString+="<h5>" + data[0].name + "</h5>";
-          htmlString+="<p class='title'>" + data[0].title + "<p>";
-          htmlString+="<p>" + data[0].about + "</p>";
+          htmlString+="<h5>" + data[1].name + "</h5>";
+          htmlString+="<p class='title'>" + data[1].title + "<p>";
+          htmlString+="<p>" + data[1].about + "</p>";
           personalInfo.innerHTML = htmlString;    
           
           ourData = ""
@@ -37,12 +37,8 @@ $(document).ready(function() {
       function renderCommentHTML(data){
            for(let i= 0; i< data.length ;i++){
              var text = "";
-             text+= "   <div class='col-md-5 offset-1'><p>" +data[1].comment +
-              " <span>  <i id='like' class='fas fa-angle-double-up'>" +
-              "<i id='likeCounter' class='text-normal'>13</i></i>" +
-              "<i id='dislike' class='fas fa-angle-double-down'>"+
-              "<i id='dislikeCounter' class='text-normal'>2</i></i>"+
-              "</span></p></div>";              
+             text+= " <div class='col-md-5 offset-1'><p>" 
+             + data[i].comment +"</p></div>";              
               comText.innerHTML = text;
 
               textData = ""
